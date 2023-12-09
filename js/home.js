4898e6393c4dfe010d9e49c5b79ff42f3d229c37
BalencedVeil717 <abhinavkumar150807@gmail.com>
2023-12-09 15:12:49 +0530

    Addition of types page
    Some content pending

--- js/home.js
+++ js/home.js
@@ -10,5 +10,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
+  setTimeout(carousel, 4000);
 }
